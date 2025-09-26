09f5cc0fa8812dc779c63cdeff49b4a4b947b0b7
NaN <mauriciodominguez9876543@gmail.com>
2025-09-26 18:46:48 -0300

    Agregando proyecto test con su test rojo

---
+++ test/GustosApp.API.Tests/GustoControllerTest.cs
@@ -0,0 +1,20 @@
+using GustosApp.API.Controllers;
+
+namespace ApiTest
+{
+    public class GustoControllerTest
+    {
+        private readonly GustoController gustoController = new GustoController();
+        //como probar con esto? vi que se puede hacer asi y tener unica instancia 
+        //o crear un http client cual va a ser el aprobado por la catedra
+        //tmb podria ser perfectamente un mock o un metodo que se inicie antes de cada prueba tipo before para ahcer setup
+
+        [Fact]
+        public void Test1()
+        {
+            string resultado = gustoController.Hola();
+
+            Assert.Equal("Hola NO soy GustoApp", resultado);
+        }
+    }
+}

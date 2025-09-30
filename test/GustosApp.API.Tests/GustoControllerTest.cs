acce5f1e93405e8eb591c9d85da76c6548fd715c
NaN <mauriciodominguez9876543@gmail.com>
2025-09-30 15:51:40 -0300

    testing continuos integracion with reusable workflow

--- test/GustosApp.API.Tests/GustoControllerTest.cs
+++ test/GustosApp.API.Tests/GustoControllerTest.cs
@@ -7,7 +7,7 @@ namespace ApiTest
         private readonly GustoController gustoController = new GustoController();
         //como probar con esto? vi que se puede hacer asi y tener unica instancia 
         //o crear un http client cual va a ser el aprobado por la catedra
-        //tmb podria ser perfectamente un mock o un metodo que se inicie antes de cada prueba tipo before para ahcer setup
+        //tmb podria ser perfectamente un mock o un metodo que se inicie antes de cada prueba tipo before para hacer setup
 
         [Fact]
         public void Test1()
